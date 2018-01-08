aebb1f6a9acd25368034bbdf4b1f240edf3b7498
Jérôme Comte <jcomte@denkbar.io>
2018-01-08 16:09:42 +0100

    Reorganizing project structure

--- examples/my-keyword-library/index.js
+++ examples/my-keyword-library/index.js
@@ -1,9 +1,10 @@
-var runner = require("step-agent-node").runner({"keywords":"keywords/keywords.js"});
+var runner = require("step-node-agent").runner({"keywords":"keywords/keywords.js"});
 const assert = require('assert');
 
 (async () => {
 	await runner.run("Open_Chrome",{});
 	var output = await runner.run("Google_Search",{"search":"djigger"});
 	assert.equal(output.payload.result,'OK');
-	await runner.run("Google_Search",{"search":"djigger2"});
+	await runner.run("Google_Search",{"search":"step"});
+	assert.equal(output.payload.result,'OK');
 })();
